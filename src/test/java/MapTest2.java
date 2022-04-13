b6cb9a3e8ba7377bf449e7973a71bad7c3e4f770
AndreaPepe <pepe.andmj@gmail.com>
2022-04-13 18:14:01 +0200

    First commit with pom configured; TODO: 1) Setup GithubActions 2) Setup SonarCloud

---
+++ src/test/java/MapTest2.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import org.junit.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import org.junit.Test;
+
+
+public class MapTest2 extends TestCase {
+    @Test
+    public void test_map () throws Exception {
+        Map<Object, Object> map = JSON.parseObject("{1:\"2\",\"3\":4,'5':6}", new TypeReference<Map<Object, Object>>() {});
+        Assert.assertEquals("2", map.get(1));
+        Assert.assertEquals(4, map.get("3"));
+        Assert.assertEquals(6, map.get("5"));
+    }
+}
