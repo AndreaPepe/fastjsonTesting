2ba9dfebb6ef0706fd55c5e7330b55b4ed61258d
AndreaPepe <pepe.andmj@gmail.com>
2022-04-14 23:25:51 +0200

    Test classes have been parameterized. Although, it can be useful to check if 'typeReference' parameter in JSONArrayParseTest.java should be more general

--- src/test/java/JSONArrayParseTest.java
+++ src/test/java/JSONArrayParseTest.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -6,13 +8,54 @@ import junit.framework.TestCase;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class JSONArrayParseTest extends TestCase {
+
+    /**
+    Old Test
+
     public void test_array() throws Exception {
         String text = "[{id:123}]";
         List<Map<String, Integer>> array = JSON.parseObject(text, new TypeReference<List<Map<String, Integer>>>() {});
         Assert.assertEquals(1, array.size());
         Map<String, Integer> map  = array.get(0);
         Assert.assertEquals(123, map.get("id").intValue());
+    }*/
+
+    /* The instance to be tested is not needed because the tested method is static*/
+    private String jsonArray;
+    private TypeReference<List<Map<String, Integer>>> typeReference;
+    private int expectedOne;
+    private int expectedTwo;
+
+    public JSONArrayParseTest(String jsonArray, TypeReference<List<Map<String, Integer>>> typeReference, int expectedOne, int expectedTwo){
+        configureTestClass(jsonArray, typeReference, expectedOne, expectedTwo);
+    }
+
+    private void configureTestClass(String jsonArray, TypeReference<List<Map<String, Integer>>> typeReference, int expectedOne, int expectedTwo){
+        this.jsonArray = jsonArray;
+        this.typeReference = typeReference;
+        this.expectedOne = expectedOne;
+        this.expectedTwo = expectedTwo;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getParameters() {
+        return Arrays.asList( new Object[][]{
+                // jsonArray, typeReference, expectedOne, expectedTwo
+                {"[{id:123}]", new TypeReference<List<Map<String, Integer>>>() {}, 1, 123}
+        });
+    }
+
+    @Test
+    public void test_array() throws Exception {
+        List<Map<String, Integer>> array = JSON.parseObject(this.jsonArray, this.typeReference);
+        Assert.assertEquals(this.expectedOne, array.size());
+        Map<String, Integer> map  = array.get(0);
+        Assert.assertEquals(this.expectedTwo, map.get(map.keySet().iterator().next()).intValue());
     }
 }
--- src/test/java/MapTest2.java
+++ src/test/java/MapTest2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import org.junit.Assert;
 import junit.framework.TestCase;
@@ -5,14 +7,53 @@ import junit.framework.TestCase;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-
+@RunWith(Parameterized.class)
 public class MapTest2 extends TestCase {
-    @Test
+    /*
+    Old test case
+
     public void test_map () throws Exception {
         Map<Object, Object> map = JSON.parseObject("{1:\"2\",\"3\":4,'5':6}", new TypeReference<Map<Object, Object>>() {});
         Assert.assertEquals("2", map.get(1));
         Assert.assertEquals(4, map.get("3"));
         Assert.assertEquals(6, map.get("5"));
+    }
+     */
+
+    /* The instance to be tested is not needed because the tested method is static*/
+
+    private String jsonObject;
+    private TypeReference<Map<Object,Object>> typeReference;
+    private Object[] keys;
+    private Object[] expectedValues;
+
+    public MapTest2 (String jsonObject, TypeReference<Map<Object,Object>> typeReference, Object[] keys, Object[] expectedValues){
+        configureTestClass(jsonObject, typeReference, keys, expectedValues);
+    }
+
+    private void configureTestClass(String jsonObject, TypeReference<Map<Object,Object>> typeReference, Object[] keys, Object[] expectedValues){
+        this.jsonObject = jsonObject;
+        this.typeReference = typeReference;
+        this.keys = keys;
+        this.expectedValues = expectedValues;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getParameters(){
+        return Arrays.asList(new Object[][] {
+                // jsonObject (string), typeReference, expected (array)
+                {"{1:\"2\",\"3\":4,'5':6}", new TypeReference<Map<Object, Object>>() {}, new Object[]{1, "3", "5"}, new Object[]{"2", 4, 6} }
+        });
+    }
+
+    @Test
+    public void test_map () throws Exception {
+        Map<Object, Object> map = JSON.parseObject(this.jsonObject, this.typeReference);
+        for (int i = 0; i < this.keys.length; i++){
+            Assert.assertEquals(this.expectedValues[i], map.get(this.keys[i]));
+        }
     }
 }
