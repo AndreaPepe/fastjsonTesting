8b5a5c3c9ad94fe6b5ceba845b4dfb96af2a0f6c
AndreaPepe <pepe.andmj@gmail.com>
2022-06-13 17:00:16 +0200

    Tests correctly parametrized but one test case run per class can be made

--- src/test/java/JSONPatchTest_0.java
+++ src/test/java/JSONPatchTest_0.java
@@ -1,12 +1,6 @@
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONPatch;
-import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 /*
 
@@ -96,49 +90,48 @@ public class JSONPatchTest_0 extends TestCase {
 }
 */
 
-@RunWith(Parameterized.class)
-public class JSONPatchTest_0 extends TestCase{
+public class JSONPatchTest_0 {
 
     /* Instance under test is not needed, because the tested method is static*/
     private String original;
     private String patch;
     private String expected;
 
-    public JSONPatchTest_0 (String original, String patch, String expected){
-        configureTestClass(original, patch, expected);
+    public JSONPatchTest_0() {
+        configureTestClass("{\n" + "  \"baz\": \"qux\",\n" + "  \"foo\": \"bar\"\n" + "}",
+                  "[\n" +
+                        "  { \"op\": \"replace\", \"path\": \"/baz\", \"value\": \"boo\" },\n" +
+                        "  { \"op\": \"add\", \"path\": \"/hello\", \"value\": [\"world\"] },\n" +
+                        "  { \"op\": \"remove\", \"path\": \"/foo\" }\n" +
+                        "]",
+                "{\"baz\":\"boo\",\"hello\":[\"world\"]}");
+
+//        configureTestClass("{}", "{ \"op\": \"add\", \"path\": \"/a/b/c\", \"value\": [ \"foo\", \"bar\" ] }",
+//                "{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}");
+//
+//        configureTestClass("{}", "{ \"op\": \"remove\", \"path\": \"/a/b/c\" }\n",
+//                "{}");
+//        configureTestClass("{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"remove\", \"path\": \"/a/b/c\" }\n",
+//                "{\"a\":{\"b\":{}}}");
+//        configureTestClass("{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"replace\", \"path\": \"/a/b/c\", \"value\": 42 }",
+//                "{\"a\":{\"b\":{\"c\":42}}}");
+//        configureTestClass("{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"move\", \"from\": \"/a/b/c\", \"path\": \"/a/b/d\" }",
+//                "{\"a\":{\"b\":{\"d\":[\"foo\",\"bar\"]}}}");
+//        configureTestClass("{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"copy\", \"from\": \"/a/b/c\", \"path\": \"/a/b/e\" }",
+//                "{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"],\"e\":[\"foo\",\"bar\"]}}}");
+//        configureTestClass("{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"test\", \"path\": \"/a/b/c\", \"value\": \"foo\" }",
+//                "false");
     }
 
-    private void configureTestClass(String original, String patch, String expected){
+    private void configureTestClass(String original, String patch, String expected) {
         this.original = original;
         this.patch = patch;
         this.expected = expected;
     }
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> getParameters(){
-        return Arrays.asList(new Object[][]{
-                // original, patch, expected
-                {"{\n" +
-                        "  \"baz\": \"qux\",\n" +
-                        "  \"foo\": \"bar\"\n" +
-                        "}", "[\n" +
-                        "  { \"op\": \"replace\", \"path\": \"/baz\", \"value\": \"boo\" },\n" +
-                        "  { \"op\": \"add\", \"path\": \"/hello\", \"value\": [\"world\"] },\n" +
-                        "  { \"op\": \"remove\", \"path\": \"/foo\" }\n" +
-                        "]", "{\"baz\":\"boo\",\"hello\":[\"world\"]}"},
-                {"{}", "{ \"op\": \"add\", \"path\": \"/a/b/c\", \"value\": [ \"foo\", \"bar\" ] }", "{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}"},
-                {"{}", "{ \"op\": \"remove\", \"path\": \"/a/b/c\" }\n", "{}"},
-                {"{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"remove\", \"path\": \"/a/b/c\" }\n", "{\"a\":{\"b\":{}}}"},
-                {"{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"replace\", \"path\": \"/a/b/c\", \"value\": 42 }", "{\"a\":{\"b\":{\"c\":42}}}"},
-                {"{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"move\", \"from\": \"/a/b/c\", \"path\": \"/a/b/d\" }", "{\"a\":{\"b\":{\"d\":[\"foo\",\"bar\"]}}}"},
-                {"{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"copy\", \"from\": \"/a/b/c\", \"path\": \"/a/b/e\" }", "{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"],\"e\":[\"foo\",\"bar\"]}}}"},
-                {"{\"a\":{\"b\":{\"c\":[\"foo\",\"bar\"]}}}", "{ \"op\": \"test\", \"path\": \"/a/b/c\", \"value\": \"foo\" }", "false"}
-        });
-    }
-
     @Test
-    public void genericTest() throws Exception {
+    public void testJSONPatch() {
         String result = JSONPatch.apply(original, patch);
-        assertEquals(expected, result);
+        Assert.assertEquals(expected, result);
     }
 }
--- src/test/java/MapTest2.java
+++ src/test/java/MapTest2.java
@@ -1,17 +1,14 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Map;
-import org.junit.Assert;
-import junit.framework.TestCase;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-@RunWith(Parameterized.class)
-public class MapTest2 extends TestCase {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MapTest2 {
     /*
     Old test case
 
@@ -28,32 +25,60 @@ public class MapTest2 extends TestCase {
     private String jsonObject;
     private TypeReference<Map<Object,Object>> typeReference;
     private Object[] keys;
-    private Object[] expectedValues;
 
-    public MapTest2 (String jsonObject, TypeReference<Map<Object,Object>> typeReference, Object[] keys, Object[] expectedValues){
-        configureTestClass(jsonObject, typeReference, keys, expectedValues);
+    public MapTest2 (){
+        configureTestClass("{1:\"2\",\"3\":4,'5':6}", new Object[]{1, "3", "5"});
+
+        // this has been added by me
+//        configureTestClass("{\"abc\":\"hello\",\"3\":4,5:\"null\"}", new Object[]{"abc", "3", 5});
     }
 
-    private void configureTestClass(String jsonObject, TypeReference<Map<Object,Object>> typeReference, Object[] keys, Object[] expectedValues){
+    private void configureTestClass(String jsonObject, Object[] keys){
         this.jsonObject = jsonObject;
-        this.typeReference = typeReference;
+        this.typeReference = new TypeReference<>() {};
         this.keys = keys;
-        this.expectedValues = expectedValues;
     }
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> getParameters(){
-        return Arrays.asList(new Object[][] {
-                // jsonObject (string), typeReference, expected (array)
-                {"{1:\"2\",\"3\":4,'5':6}", new TypeReference<Map<Object, Object>>() {}, new Object[]{1, "3", "5"}, new Object[]{"2", 4, 6} }
-        });
-    }
-
+    /**
+     * This test method invokes the <i>parseObject</i> static method of the JSON class.
+     * In particular, it checks that the conversion from the string representation of the jsonObject to a Map
+     * between keys and values is well done.
+     * The test is passed only if each value is associated to the correct key.
+     */
     @Test
-    public void test_map () throws Exception {
+    public void test_map () {
         Map<Object, Object> map = JSON.parseObject(this.jsonObject, this.typeReference);
+        Oracle oracle = new Oracle();
+        Object[] expected = oracle.getExpectedResults(jsonObject, keys);
+        boolean passed = true;
         for (int i = 0; i < this.keys.length; i++){
-            Assert.assertEquals(this.expectedValues[i], map.get(this.keys[i]));
+            if (!map.get(keys[i]).equals(expected[i])){
+                passed = false;
+                break;
+            }
+        }
+        Assert.assertTrue(passed);
+    }
+
+
+
+    private static class Oracle{
+        /**
+         * This private method is used as a Software Testing oracle to know the
+         * expected values of the JSON Object referred by the key in the input array of keys.
+         * It's an oracle because it uses the library <b>org.json</b> as a trusted entity.
+         *
+         * @param jsonObject The string representing the JSONObject of the FastJSON project
+         * @param keys The array of the objects representing the keys of the JSON Object
+         * @return an array of Object, containing the values expected to be contained in the jsonObject, in the same order of their keys
+         */
+        private Object[] getExpectedResults(String jsonObject, Object[] keys){
+            JSONObject obj = new JSONObject(jsonObject);
+            List<Object> expected = new LinkedList<>();
+            for (int i = 0; i < keys.length; i++){
+                expected.add(i, obj.get(String.valueOf(keys[i])));
+            }
+            return expected.toArray();
         }
     }
 }
